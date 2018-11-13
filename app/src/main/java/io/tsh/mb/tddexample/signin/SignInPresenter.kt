1507e1d191ef8c2bbf8cb5c7e39841cd4abc2a97
Michal Bujalski <michal@tsh.io>
2018-11-13 22:25:17 +0100

    Sign in presenter refactoring

--- app/src/main/java/io/tsh/mb/tddexample/signin/SignInPresenter.kt
+++ app/src/main/java/io/tsh/mb/tddexample/signin/SignInPresenter.kt
@@ -1,7 +1,9 @@
 package io.tsh.mb.tddexample.signin
 
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.internal.disposables.ArrayCompositeDisposable
+import io.reactivex.rxkotlin.addTo
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.Schedulers
 import io.tsh.mb.tddexample.R
@@ -11,34 +13,34 @@ class SignInPresenter(
     private val loginRepository: SignInRepository,
     private val loginValidator: SignInValidator
 ): SignInContract.Presenter {
+    val compositeDisposable = CompositeDisposable()
     override fun login(username: String, password: String) {
         val validationResult = loginValidator.validate(username, password)
-        if (validationResult == ValidationResult.VALID) {
-            view.showProgress()
-            loginRepository
-                .login(username, password)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeBy(
-                    onComplete = {
-                        view.hideProgress()
-                        view.onLoginSuccess()
-                    },
-                    onError = {
-                        view.hideProgress()
-                        if (it is NoConnectionException) {
-                            view.showNoConnectionError()
-                        } else if (it is ApiException) {
-                            view.showError(it.msg)
+        when (validationResult) {
+            ValidationResult.VALID -> {
+                view.showProgress()
+                loginRepository
+                    .login(username, password)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribeBy(
+                        onComplete = {
+                            view.hideProgress()
+                            view.onLoginSuccess()
+                        },
+                        onError = {
+                            view.hideProgress()
+                            if (it is NoConnectionException) {
+                                view.showNoConnectionError()
+                            } else if (it is ApiException) {
+                                view.showError(it.msg)
+                            }
                         }
-                    }
-                )
-        }else if( validationResult == ValidationResult.USERNAME_EMPTY ){
-            view.showUsernameError(R.string.signInErrorUsernameEmpty)
-        }else if( validationResult == ValidationResult.PASSWORD_EMPTY ){
-            view.showPasswordError(R.string.signInPasswordEmpty)
-        }else if( validationResult == ValidationResult.PASSWORD_NOT_VALID ){
-            view.showPasswordError(R.string.signInErrorPasswordNotValid)
+                    ).addTo(compositeDisposable)
+            }
+            ValidationResult.USERNAME_EMPTY -> view.showUsernameError(R.string.signInErrorUsernameEmpty)
+            ValidationResult.PASSWORD_EMPTY -> view.showPasswordError(R.string.signInPasswordEmpty)
+            ValidationResult.PASSWORD_NOT_VALID -> view.showPasswordError(R.string.signInErrorPasswordNotValid)
         }
     }
 }
