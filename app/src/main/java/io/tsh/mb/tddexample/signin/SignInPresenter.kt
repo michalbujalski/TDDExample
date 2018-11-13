d3c75d942114d61837ffde2a8c630eb909989b18
Michal Bujalski <michal@tsh.io>
2018-11-13 22:23:54 +0100

    Sign in presenter implementation

--- app/src/main/java/io/tsh/mb/tddexample/signin/SignInPresenter.kt
+++ app/src/main/java/io/tsh/mb/tddexample/signin/SignInPresenter.kt
@@ -1,8 +1,10 @@
 package io.tsh.mb.tddexample.signin
 
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.internal.disposables.ArrayCompositeDisposable
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.Schedulers
+import io.tsh.mb.tddexample.R
 
 class SignInPresenter(
     private val view: SignInContract.View,
@@ -10,8 +12,33 @@ class SignInPresenter(
     private val loginValidator: SignInValidator
 ): SignInContract.Presenter {
     override fun login(username: String, password: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val validationResult = loginValidator.validate(username, password)
+        if (validationResult == ValidationResult.VALID) {
+            view.showProgress()
+            loginRepository
+                .login(username, password)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeBy(
+                    onComplete = {
+                        view.hideProgress()
+                        view.onLoginSuccess()
+                    },
+                    onError = {
+                        view.hideProgress()
+                        if (it is NoConnectionException) {
+                            view.showNoConnectionError()
+                        } else if (it is ApiException) {
+                            view.showError(it.msg)
+                        }
+                    }
+                )
+        }else if( validationResult == ValidationResult.USERNAME_EMPTY ){
+            view.showUsernameError(R.string.signInErrorUsernameEmpty)
+        }else if( validationResult == ValidationResult.PASSWORD_EMPTY ){
+            view.showPasswordError(R.string.signInPasswordEmpty)
+        }else if( validationResult == ValidationResult.PASSWORD_NOT_VALID ){
+            view.showPasswordError(R.string.signInErrorPasswordNotValid)
+        }
     }
-
-
 }
