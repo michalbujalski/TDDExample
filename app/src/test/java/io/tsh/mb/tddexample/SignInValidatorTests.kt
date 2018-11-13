a15a84f8d05c7fa977353c8032ae19a1b19ed19b
Michal Bujalski <michal@tsh.io>
2018-11-13 21:50:26 +0100

    Sign in validation test cases

--- app/src/test/java/io/tsh/mb/tddexample/SignInValidatorTests.kt
+++ app/src/test/java/io/tsh/mb/tddexample/SignInValidatorTests.kt
@@ -21,5 +21,60 @@ class SignInValidatorTests {
     fun setUp(){
         validator = SignInValidatorImpl()
     }
+
+    @Test
+    fun `validate username empty, password empty`(){
+        //given
+        val username = ""
+        val password = ""
+        //when
+        val result = validator.validate(username,password)
+        //then
+        Assert.assertEquals(ValidationResult.USERNAME_EMPTY,result)
+    }
+
+    @Test
+    fun `validate username valid, password empty`(){
+        //given
+        val username = "someUsername!@#"
+        val password = ""
+        //when
+        val result = validator.validate(username,password)
+        //then
+        Assert.assertEquals(ValidationResult.PASSWORD_EMPTY,result)
+    }
+
+    @Test
+    fun `validate username not valid, password empty`(){
+        //given
+        val username = "s"
+        val password = ""
+        //when
+        val result = validator.validate(username,password)
+        //then
+        Assert.assertEquals(ValidationResult.USERNAME_NOT_VALID,result)
+    }
+
+    @Test
+    fun `validate username not valid, password not valid`(){
+        //given
+        val username = "s"
+        val password = "asdf"
+        //when
+        val result = validator.validate(username,password)
+        //then
+        Assert.assertEquals(ValidationResult.USERNAME_NOT_VALID,result)
+    }
+
+    @Test
+    fun `validate username valid, password valid`(){
+        //given
+        val username = "usernam01"
+        val password = "someLongPassword123"
+        //when
+        val result = validator.validate(username,password)
+        //then
+        Assert.assertEquals(ValidationResult.VALID,result)
+    }
 }
 
